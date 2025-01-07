60ddf411c2e7518911981ab9fe0d1e0972d8091e
abhijeetsenguptaa <senguptabhijeet@gmailcom>
2025-01-07 16:59:31 +0530

    complete filter

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ const seriesRoutes = require('./routers/series.routes');
 const seriesVideoRoutes = require('./routers/seriesVideo.routes');
 const audioRoutes = require('./routers/audio.routes');
 const urlRoutes = require('./routers/url.routes');
+const SpeakersModel = require('./models/speakers.model');
+const ContentModel = require('./models/content.model');
+const AudioModel = require('./models/audio.model');
+const { Op } = require('sequelize');
 
 
 
@@ -60,8 +64,58 @@ app.use('/api/languages', languageRoute);
 app.use('/api/banners', bannerRoutes);
 app.use('/api/reels', reelsRoutes);
 app.use('/api/url', urlRoutes);
+app.get('/api/search', async (req, res) => {
+    try {
+        const { title } = req.query;
 
+        if (!title) {
+            return res.status(400).json({
+                message: "Title parameter is required."
+            });
+        }
 
+        console.log(`Search title: ${title}`);
+
+        // Case-insensitive search for titles
+        const speakers = await SpeakersModel.findAll({
+            where: {
+                title: {
+                    [Op.like]: `%${title}%`,
+                }
+            }
+        });
+
+        const videos = await ContentModel.findAll({
+            where: {
+                title: {
+                    [Op.like]: `%${title}%`,
+                }
+            }
+        });
+
+        const audios = await AudioModel.findAll({
+            where: {
+                title: {
+                    [Op.like]: `%${title}%`,
+                }
+            }
+        });
+
+        return res.status(200).json({
+            message: "Data retrieved based on the search criteria.",
+            data: {
+                speakers,
+                videos,
+                audios
+            }
+        });
+    } catch (error) {
+        console.error('Error during search operation:', error);
+        return res.status(500).json({
+            message: "An error occurred while processing your search request. Please try again later."
+        });
+    }
+});
 
 // Synchronize the database connection and start the server
 connection.sync().then(() => {
